8830bcc5423169f84a9fcd21406b3b18274cf0c9
kunitoshi291 <takevadogu@outlook.jp>
2022-07-27 15:19:29 +0000

    permitメソッドで、DBに不正な値が保存されるのを防ぐ。また、question_paramsメソッドで取得できるパラメータを使ってDBに値を保存するところまで

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,6 +16,12 @@ class QuestionsController < ApplicationController
   
   # 質問の登録
   def create
+    # Questionモデルを初期化
+    @question = Question.new(question_params)
+    # QuestionモデルをDBへ保存
+    @question.save
+    # showへリダイレクト
+    redirect_to @question
   end
   
   # 質問の編集
@@ -29,6 +35,13 @@ class QuestionsController < ApplicationController
   
   # 質問の削除
   def delete
+  end
+  
+  private
+  def question_params
+    # requireメソッドで、データのオブジェクト名を指定し、permitメソッドで、DB保存処理時のカラムを指定
+    # permitメソッドで許可していない項目に関しては、DBの値の変更には使われなくなるため、安全にデータを扱える
+    params.require(:question).permit(:title, :name, :content)
   end
 end
 
