70d4978bcba38ae2423568847e02bbcc40ec63f8
kunitoshi291 <takevadogu@outlook.jp>
2022-07-30 06:00:47 +0000

    質問更新時の機能と、質問削除時の機能の追加

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -22,22 +22,34 @@ class QuestionsController < ApplicationController
     # Questionモデルを初期化
     @question = Question.new(question_params)
     # QuestionモデルをDBへ保存
-    @question.save
-    # showへリダイレクト
-    redirect_to @question
+    if @question.save
+      # showへリダイレクト
+      redirect_to @question
+    else
+      render 'new', status: :unprocessable_entity
+    end
   end
-  
   # 質問の編集
   def edit
+    @question = Question.find(params[:id])
   end
   
   # 質問の更新
   def update
-    
+    @question = Question.find(params[:id])
+    # updateメソッドの引数にquestion_paramsをつけることにより、許可したカラムのみ更新する
+    if @question.update(question_params)
+      redirect_to @question
+    else
+      render 'edit', status: :unprocessable_entity
+    end
   end
   
   # 質問の削除
-  def delete
+  def destroy
+    @question = Question.find(params[:id])
+    @question.destroy
+    redirect_to questions_path
   end
   
   private
@@ -47,4 +59,3 @@ class QuestionsController < ApplicationController
     params.require(:question).permit(:title, :name, :content)
   end
 end
-
