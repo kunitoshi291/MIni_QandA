fcc63c85053590d2809ba80f30f229adaa2ffda7
Takekuni <takevadogu@outlook.jp>
2022-08-04 23:56:50 +0900

    has_manyで、関連質問に対する回答も削除できるようにする。

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
     # has_many→他のモデルとの間に1対多のつながりがあることを証明
-    hash_many :answers
+    hash_many :answers, dependent: :destroy  # 関連する質問の回答も削除できるようにする
     validates :title, presence: true
     validates :name, presence: true
     validates :content, presence: true, length: {minimum: 5}
